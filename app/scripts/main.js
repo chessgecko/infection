39b7ad8e41bbdc852459a1df961f0b5a2563b88e
David Silin <DSilin17@gmail.com>
2014-09-20 01:03:33 -0500

    to server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,49 @@
 //$( "#taggedText" ).css("top", "50px");
 
+var killerNum = 0;
+var myNum = 1;
+var myName;
+var killerName;
+var url = document.URL;
 
-// $( "#taggedButton" ).on("click", function() {
-// 	//$( "#taggedText" ).slideUp();
+// alert(url);
+
+while(!isNaN(parseInt(url.charAt(url.length-1)))) {
+	killerNum*=10;
+	killerNum+=parseInt(url.charAt(url.length-1));
+	url = url.substring(0,url.length-1);
+}
+alert(killerNum);
+
+// $.get("http://myrighttoplay.com/hackathon/zombie.php?killer=1"/* + killerNum*/).done(function(data){
+// 	killerNum = data.killNum;
+// 	killerName = data.killerName;
+// 	setupText();
 // });
 
-function initialize() {
-	var mapOptions = {
-		center: { lat: -34.397, lng: 150.644},
-		zoom: 8
-	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'),
-		mapOptions);
+$.getJSON('http://myrighttoplay.com/hackathon/zombie.php?killer=1', function(data) {
+    console.log(data);
+});
+
+
+function setupText(){
+	if(killerName){
+		$('#KillerNamed').html('Zombie #' + killerNum + ' AKA ' + killerName + ' HAS EATEN YOUR BRAINS');
+	} else {
+		$('#KillerNamed').html('Zombie #' + killerNum + ' AKA ' + " Jake the Ripper" + ' HAS EATEN YOUR BRAINS');
+	}
+
+	$('#myLink').html("http://SUPERORGANDONOR.org/"+killerNum);
+	$("#myProgress").html("http://SUPERORGANDONOR.org/progress/"+killerNum);
+	$("#YouNamed").html("Welcome to the winning team zombie #" + myNum + " or better yet...");
+
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+
+$('#taggedName').keypress(function(e) {
+	if(e.which == 13) {
+		$('#YouNamed').html("Welcome to the winning team " + $("#taggedName" ).val() + ". You are Zombie #" + myNum + ".");
+		myName = $("#taggedName" ).val();
+		$('#taggedName').css("visibility", "hidden");
+	}
+});
